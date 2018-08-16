0cc41fff720d2f2dcd73249d19d0d513166c146d
carl van schie <carl.vanschie@gmail.com>
2018-08-16 19:55:15 +0100

    Initial healthcheck done.

---
+++ acceptance-test/src/test/java/com/vanschie/acceptancetest/AcceptanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanschie.acceptancetest;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class AcceptanceTestApplicationTests {
+
+
+}
+
---
+++ acceptance-test/src/test/java/com/vanschie/acceptancetest/HealthCheckSteps.java
@@ -0,0 +1,28 @@
+package com.vanschie.acceptancetest;
+
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class HealthCheckSteps {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private ResponseEntity<String> entity;
+
+    @When("^the client calls the health check endpoint$")
+    public void theClientCallsTheHealthCheckEndpoint() {
+        entity = restTemplate.getForEntity("http://localhost:8080/actuator/health", String.class);
+    }
+
+    @Then("^the client receives status code of (\\d+)$")
+    public void theClientReceivesStatusCodeOf(int statusCode) {
+        assertThat(entity.getStatusCode().value(), is(statusCode));
+    }
+}
